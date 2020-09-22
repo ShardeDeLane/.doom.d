ef34df57a88d054dffb915751549af5fc5158366
Sharde DeLane <rhcprocker@yahoo.com>
2020-09-22 04:08:15 -0500

    Update config.el

--- config.el
+++ config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
- (setq doom-font (font-spec :family "monaco" :size 13 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "monaco" :size 13))
+(setq doom-font (font-spec :family "monaco" :size 13 :weight 'semi-light)
+       doom-variable-pitch-font (font-spec :family "monaco" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -35,17 +35,26 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-;; centaur tab config
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
 (use-package centaur-tabs
    :load-path "~/.emacs.d/other/centaur-tabs"
    :config
    (setq centaur-tabs-style "bar"
-      centaur-tabs-height 32
-      centaur-tabs-set-icons t
-      centaur-tabs-set-modified-marker t
-      centaur-tabs-show-navigation-buttons t
-      centaur-tabs-set-bar 'under
-      x-underline-at-descent-line t)
+        centaur-tabs-height 32
+        centaur-tabs-set-icons t
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-show-navigation-buttons t
+        centaur-tabs-set-bar 'under
+        x-underline-at-descent-line t)
    (centaur-tabs-headline-match)
    ;; (setq centaur-tabs-gray-out-icons 'buffer)
    ;; (centaur-tabs-enable-buffer-reordering)
@@ -61,38 +70,38 @@
  Other buffer group by `centaur-tabs-get-group-name' with project name."
      (list
       (cond
-    ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
-    ;; "Remote")
-    ((or (string-equal "*" (substring (buffer-name) 0 1))
-         (memq major-mode '(magit-process-mode
-                magit-status-mode
-                magit-diff-mode
-                magit-log-mode
-                magit-file-mode
-                magit-blob-mode
-                magit-blame-mode
-                )))
-     "Emacs")
-    ((derived-mode-p 'prog-mode)
-     "Editing")
-    ((derived-mode-p 'dired-mode)
-     "Dired")
-    ((memq major-mode '(helpful-mode
-                help-mode))
-     "Help")
-    ((memq major-mode '(org-mode
-                org-agenda-clockreport-mode
-                org-src-mode
-                org-agenda-mode
-                org-beamer-mode
-                org-indent-mode
-                org-bullets-mode
-                org-cdlatex-mode
-                org-agenda-log-mode
-                diary-mode))
-     "OrgMode")
-    (t
-     (centaur-tabs-get-group-name (current-buffer))))))
+      ;; ((not (eq (file-remote-p (buffer-file-name)) nil))
+      ;; "Remote")
+      ((or (string-equal "*" (substring (buffer-name) 0 1))
+           (memq major-mode '(magit-process-mode
+                        magit-status-mode
+                        magit-diff-mode
+                        magit-log-mode
+                        magit-file-mode
+                        magit-blob-mode
+                        magit-blame-mode
+                        )))
+       "Emacs")
+      ((derived-mode-p 'prog-mode)
+       "Editing")
+      ((derived-mode-p 'dired-mode)
+       "Dired")
+      ((memq major-mode '(helpful-mode
+                      help-mode))
+       "Help")
+      ((memq major-mode '(org-mode
+                      org-agenda-clockreport-mode
+                      org-src-mode
+                      org-agenda-mode
+                      org-beamer-mode
+                      org-indent-mode
+                      org-bullets-mode
+                      org-cdlatex-mode
+                      org-agenda-log-mode
+                      diary-mode))
+       "OrgMode")
+      (t
+       (centaur-tabs-get-group-name (current-buffer))))))
    :hook
    (dashboard-mode . centaur-tabs-local-mode)
    (term-mode . centaur-tabs-local-mode)
@@ -106,23 +115,12 @@
    ("C-c t p" . centaur-tabs-group-by-projectile-project)
    ("C-c t g" . centaur-tabs-group-buffer-groups)
    (:map evil-normal-state-map
-      ("g t" . centaur-tabs-forward)
-      ("g T" . centaur-tabs-backward)))
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
+        ("g t" . centaur-tabs-forward)
+        ("g T" . centaur-tabs-backward)))
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
